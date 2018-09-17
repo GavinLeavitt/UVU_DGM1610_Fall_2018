598ba6626e766a1fd167d2687be0d614e87f9bd5
Gavin Leavitt <gavin.leavitt@gmail.com>
2018-09-17 17:18:38 -0600

    Created TestLevel and made functioning movement script

--- Game_Fall2018/Assets/Scripts/CharacterMove.cs
+++ Game_Fall2018/Assets/Scripts/CharacterMove.cs
@@ -30,7 +30,14 @@ public class CharacterMove : MonoBehaviour {
 		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 			Jump();
 		}
-		// The rest of the movement stuff will go here someday
+		// This code makes the character move from side to side using the A and D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
 	}
 
 	public void Jump(){
