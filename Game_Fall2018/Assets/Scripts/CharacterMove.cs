38af198b8deaad404f255349948aba46483ca604
Gavin Leavitt <gavin.leavitt@gmail.com>
2018-09-24 17:17:16 -0600

    Added double jump function and started ScoreManager script

--- Game_Fall2018/Assets/Scripts/CharacterMove.cs
+++ Game_Fall2018/Assets/Scripts/CharacterMove.cs
@@ -7,12 +7,17 @@ public class CharacterMove : MonoBehaviour {
 	// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight;
+	private bool DoubleJump;
 
 	// Player grounded variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+
+	//Non-Stick Player
+	private float MoveVelocity;
+
 
 	// Use this for initialization
 	void Start () {
@@ -30,14 +35,33 @@ public class CharacterMove : MonoBehaviour {
 		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 			Jump();
 		}
+
+		// Double jump code
+		if(Grounded) {
+			DoubleJump = false;
+		}
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !DoubleJump && !Grounded){
+			Jump();
+			DoubleJump = true;
+		}
+
+		//Non-Stick Player
+		MoveVelocity = 0f;
+
 		// This code makes the character move from side to side using the A and D keys
 		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-		
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = MoveSpeed;
 		}
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = -MoveSpeed;
 		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+		
 	}
 
 	public void Jump(){
