cf666b5530d75f4eae18d6c17e56a75a832565ab
Gavin Leavitt <gavin.leavitt@gmail.com>
2018-10-03 15:56:51 -0600

    Added Coin and associated scripts

--- Game_Fall2018/Assets/Scripts/ScoreManager.cs
+++ Game_Fall2018/Assets/Scripts/ScoreManager.cs
@@ -21,7 +21,7 @@ public class ScoreManager : MonoBehaviour {
 		if (Score < 0){
 			Score = 0;
 		}
-		ScoreText.text = " " + Score;
+		ScoreText.text = "            " + Score;
 	}
 
 	public static void AddPoints (int PointsToAdd) {
