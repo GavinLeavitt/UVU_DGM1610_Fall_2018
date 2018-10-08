603b2b27c6e7478aeb2b22ffdeec9bad7930cb7e
Gavin Leavitt <gavin.leavitt@gmail.com>
2018-10-08 17:16:35 -0600

    Added Calculator script and inheritance stuff

---
+++ Game_Fall2018/Assets/Scripts/Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour {
+
+	// Declare variables
+	public int Var0;
+	public int Var1;
+	private int Total;
+
+	// Various functions
+	public void Add(int One, int Two) {
+		Total = One+Two;
+	}
+
+	public void Subtract(int One, int Two) {
+		Total = One-Two;
+	}
+
+	public void Multiply(int One, int Two) {
+		Total = One*Two;
+	}
+
+	public void Divide(int One, int Two) {
+		Total = One/Two;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Events for calling the function
+		if(Input.GetKeyDown (KeyCode.A)) {
+			Add(Var0,Var1);
+			print(Total);
+		}
+
+		if(Input.GetKeyDown (KeyCode.S)) {
+			Subtract(Var0,Var1);
+			print(Total);
+		}
+
+		if(Input.GetKeyDown (KeyCode.M)) {
+			Multiply(Var0,Var1);
+			print(Total);
+		}
+
+		if(Input.GetKeyDown (KeyCode.D)) {
+			Divide(Var0,Var1);
+			print(Total);
+		}
+	}
+}
--- Game_Fall2018/Assets/Scripts/LevelManager.cs
+++ Game_Fall2018/Assets/Scripts/LevelManager.cs
@@ -4,9 +4,57 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
 
+	public GameObject CurrentCheckPoint;
+	public Rigidbody2D Player;
+
+	// Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	// Respawn Delay
+	public float RespawnDelay;
+
+	// Point Penalty on Death
+	public int PointPenaltyOnDeath;
+
+	// Store Gravity Value
+	private float GravityStore;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		// Player = FindObjectOfType<Rigidbody2D> ();
+	}
+
+	public void RespawnPlayer(){
+		StartCoroutine("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//Generate Death Particle
+		Instantiate (DeathParticle, Player.transform.position, Player.transform.rotation);
+		//Hide Player
+		// player.enabled = false;
+		Player.GetComponent<Renderer> ().enabled = false;
+		// Gravity Reset
+		GravityStore = Player.GetComponent<Rigidbody2D> ().gravityScale;
+		Player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		Player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		// Point Penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		// Debug Message
+		Debug.Log ("Player Respawn");
+		// Respawn Delay
+		yield return new WaitForSeconds (RespawnDelay);
+		// Gravity Restore
+		Player.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+		// Match Player's transform position
+		Player.transform.position = CurrentCheckPoint.transform.position;
+		//Show Player
+		// player.enabled = true;
+		Player.GetComponent<Renderer> ().enabled = true;
+		// Spawn Player
+		Instantiate (RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
 	}
 	
 	// Update is called once per frame
