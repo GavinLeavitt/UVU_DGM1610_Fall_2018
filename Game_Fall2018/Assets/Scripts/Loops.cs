47f235e952407cb3d03e284073149aa8cf1e9276
Gavin Leavitt <gavin.leavitt@gmail.com>
2018-10-24 17:15:49 -0600

    Added Loops script

---
+++ Game_Fall2018/Assets/Scripts/Loops.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	public int Num = 40;
+
+	// Use this for initialization
+	void Start () {
+		// for(int i = 0; i < 100; i++){
+		// 	print(i + " Ants Marching");
+		// }
+
+		while(Num >= 0){
+			print("The countdown is at " + Num);
+			Num--;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Game_Fall2018/Assets/Scripts/Projectile.cs
+++ Game_Fall2018/Assets/Scripts/Projectile.cs
@@ -17,7 +17,7 @@ public class Projectile : MonoBehaviour {
 	void Start () {
 
 		if(PC.transform.localScale.x < 0)
-			Speed = -Speed;	
+			Speed = -Speed;
 	}
 	
 	// Update is called once per frame
