4675f47abf9e4eeb1b8000bf2dc6f9a89c2541a6
Gavin Leavitt <gavin.leavitt@gmail.com>
2018-10-29 17:16:10 -0600

    Made 3 more For loops (homework)

--- Game_Fall2018/Assets/Scripts/Homework Scripts/LoopsHomework.cs
+++ Game_Fall2018/Assets/Scripts/Homework Scripts/LoopsHomework.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LoopsHomework : MonoBehaviour {
 
+	private int OgreSize;
+
 	// Use this for initialization
 	void Start () {
 		for (int i=0; i <= 15; i++) {
@@ -13,6 +15,18 @@ public class LoopsHomework : MonoBehaviour {
 		for (int i=99; i > 0; i--) {
 			print(i + " bottles of beer on the wall...");
 		}
+
+		for (int i=0; i < 5; i++) {
+			int OgreSize = Random.Range(2, 12);
+			print("Ogre number " + (i+1) + " is " + OgreSize + " feet tall!");
+			if(OgreSize>6) {
+				print(" Maybe you should run away?!");
+			} else if (OgreSize<4) {
+				print(" Awww... It's almost cute!");
+			}
+		}
+
+
 	}
 	
 	// Update is called once per frame
